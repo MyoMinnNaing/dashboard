db9473180c5142e4ee022cc97f72674106ee2690
MyoMinnNaing <myominnaing.webdev@gmail.com>
2024-01-16 15:57:38 +0630

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div className=" text-blue-800 text-sm font-medium">Dashboard</div>;
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    // fontSize: {
+    //   sm: "12.35px",
+    //   base: "13px",
+    //   md: "13.975px",
+    //   xl: "16.575px",
+    //   "2xl": "17.8058px",
+    //   "3xl": "2.441rem",
+    //   "4xl": "3.052rem",
+    // },
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
